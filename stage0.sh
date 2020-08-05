cf7e87af64cee3154a1403963b1ca9e9e6d648e3
kanyerezi30 <kanyerezi30@gmail.com>
2020-08-05 16:31:25 +0300

    Added my script

---
+++ stage0.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+#This is a script that prints my name, email, the language used and my biostack plus slack username (with @)
+echo "Stephen Kanyerezi" #print my name
+echo "kanyerezi30@gmail.com" #print my email
+echo "Bash" #print language used
+echo "@Kanye" #print biostack and slack username
