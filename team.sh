e2f1f25e1e0d6c7ef31b256de14d7feeeb182198
Kanyerezi30 <55234538+Kanyerezi30@users.noreply.github.com>
2020-08-06 17:53:11 +0300

    Update team.sh

--- team.sh
+++ team.sh
@@ -5,6 +5,6 @@ git clone https://github.com/Kanyerezi30/team-torsten.git #clone the team repo
 cd team-torsten #go to the cloned directory
 clear #clear the terminal
 chmod +x stage0.sh #add execute permissions
-./stage0.sh | awk -vORS=, '{print}' | sed 's/,$/\n/'
-python Added\ my\ script | awk -vORS=, '{print}' | sed 's/,$/\n/'
-
+./stage0.sh | awk -vORS=, '{print}' | sed 's/,$/\n/' #@kanye
+python Added\ my\ script | awk -vORS=, '{print}' | sed 's/,$/\n/' #@Dachoice
+python kazim | awk -vORS=, '{print}' | sed 's/,$/\n/' #@Kazim
